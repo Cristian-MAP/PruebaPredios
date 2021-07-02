441dd50e9fa074b6a3ca0cf46692bd305aa6f9c3
Cristian <cmacostap@uqvirtual.edu.co>
2021-07-01 20:59:27 -0500

    Arreglados id de las urls

--- predios/urls.py
+++ predios/urls.py
@@ -4,15 +4,15 @@ from .views import *
 urlpatterns = [
     path('agregar_predio/', AgregarPredio.as_view(), name='agregar_predio'),
     path('listar_predios/', ListarPredios.as_view(), name='listar_predios'),
-    path('actualizar_predio/<int:pk>', ActualizarPredio.as_view(), name='actualizar_predio'),
-    path('eliminar_predio/<int:pk>', EliminarPredio.as_view(), name='eliminar_predio'),
-    path('detalle_predio/<int:pk>', PredioDetalle.as_view(), name='detalle_predio'),
+    path('actualizar_predio/<str:pk>', ActualizarPredio.as_view(), name='actualizar_predio'),
+    path('eliminar_predio/<str:pk>', EliminarPredio.as_view(), name='eliminar_predio'),
+    path('detalle_predio/<str:pk>', PredioDetalle.as_view(), name='detalle_predio'),
 
     path('agregar_propietario/', AgregarPropietario.as_view(), name='agregar_propietario'),
     path('listar_propietarios/', ListarPropietarios.as_view(), name='listar_propietarios'),
-    path('actualizar_propietario/<int:pk>', ActualizarPropietario.as_view(), name='actualizar_propietario'),
-    path('eliminar_propietario/<int:pk>', EliminarPropietario.as_view(), name='eliminar_propietario'),
-    path('detalle_propietario/<int:pk>', PropietarioDetalle.as_view(), name='detalle_propietario'),
+    path('actualizar_propietario/<str:pk>', ActualizarPropietario.as_view(), name='actualizar_propietario'),
+    path('eliminar_propietario/<str:pk>', EliminarPropietario.as_view(), name='eliminar_propietario'),
+    path('detalle_propietario/<str:pk>', PropietarioDetalle.as_view(), name='detalle_propietario'),
 
     path('', Inicio.as_view(), name='inicio')
 ]
